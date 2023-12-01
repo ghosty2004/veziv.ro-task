5f8f4e7b4de75e6cfce572db15040e37211747a2
ghosty2004 <fejerszilveszter@gmail.com>
2023-12-01 13:16:49 +0200

    modal types changes

--- src/types/TModal.ts
+++ src/types/TModal.ts
@@ -1,17 +1,39 @@
-export type TModalConfirmation = {
+export type TModalButtonsTypes = 'PRIMARY' | 'SECONDARY';
+
+export type TModalConfirmationProps = {
+  title: string;
   description: string;
   buttons: Array<{
-    onClick: () => void;
+    type: TModalButtonsTypes;
+    uniqueId: string;
     label: string;
   }>;
 };
 
-export type TModalTypes = 'CONFIRMATION' | null; // maybe more types in the future...
-
-export type TModalSharedProps<T = TModalTypes> = {
-  type: T;
+export type TModalConfirmationResponse = {
+  uniqueId: string;
 };
 
+export type TModalResponseHandler<T extends TModalTypes> = {
+  handleResponse: (props: TModalResponses<T>) => void;
+};
+
+export type TModalTypes = 'CONFIRMATION' | null; // maybe more types in the future...
+
+export type TModalSharedProps<T extends TModalTypes> = {
+  type: T;
+} & Partial<TModalResponseHandler<T>>;
+
+export type TModalMethods = {
+  showConfirmation: (
+    props: TModalConfirmationProps
+  ) => Promise<TModalResponses<'CONFIRMATION'>>;
+};
+
+export type TModalResponses<T extends TModalTypes> = T extends 'CONFIRMATION'
+  ? TModalConfirmationResponse
+  : unknown;
+
 export type TModal<T = TModalTypes> = T extends 'CONFIRMATION'
-  ? TModalConfirmation & TModalSharedProps<T>
-  : TModalSharedProps<T>;
+  ? TModalConfirmationProps & TModalSharedProps<T>
+  : TModalSharedProps<null>;
