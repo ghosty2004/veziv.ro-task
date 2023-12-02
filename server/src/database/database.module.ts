f8bc250f7bd773b6c5bd3e8d624f19ce0c9474e8
ghosty2004 <fejerszilveszter@gmail.com>
2023-12-02 20:39:24 +0200

    created database module

---
+++ server/src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { User } from '../users/entities/user.entity';
+import { UsersPortfolios } from '../users-portfolios/entities/users-portfolio.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'test',
+      synchronize: process.env.NODE_ENV === 'development',
+      dropSchema: process.env.NODE_ENV === 'development',
+      entities: [User, UsersPortfolios],
+    }),
+  ],
+})
+export class DatabaseModule {}
