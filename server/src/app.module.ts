263631193b34f0ad510c35ec93c2fa035c7d0b87
ghosty2004 <fejerszilveszter@gmail.com>
2023-12-02 21:27:22 +0200

    added server static which combine the two environments server and client in one (at production level)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersPortfoliosModule } from './users-portfolios/users-portfolios.module';
+import { join } from 'node:path';
 @Module({
-  imports: [DatabaseModule, UsersModule, UsersPortfoliosModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'client', 'dist'),
+    }),
+    DatabaseModule,
+    UsersModule,
+    UsersPortfoliosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
