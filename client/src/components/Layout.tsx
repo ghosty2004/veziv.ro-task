c73712226e57d8d97484d69f6ef3426c5eb85b58
ghosty2004 <fejerszilveszter@gmail.com>
2023-12-03 19:08:16 +0200

    added navbar (for small devices)

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,12 +1,56 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdAccountCircle } from 'react-icons/md';
 import { getPages } from '@/utils';
 
-export const Header = () => {
+const Navbar = ({
+  setNavBarActive,
+}: {
+  setNavBarActive: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   const pages = getPages();
   const navigate = useNavigate();
 
-  const handlePageSwitch = (pagePath: string) => () => navigate(pagePath);
+  return (
+    <div
+      className="absolute top-0 left-0 w-full h-screen bg-black flex flex-col items-center justify-center gap-10"
+      onClick={() => setNavBarActive(false)}
+    >
+      <div data-testid="pages" className="flex flex-col gap-10 cursor-pointer">
+        {pages
+          .filter(({ visibleInHeader }) => visibleInHeader)
+          .map(({ pageName, pagePath }, index) => (
+            <button
+              key={index}
+              className="w-[120px] h-[40px] bg-gray-500 rounded-lg"
+              onClick={() => navigate(pagePath)}
+            >
+              {pageName}
+            </button>
+          ))}
+      </div>
+      <button
+        data-testid="my-account-button"
+        className="flex items-center justify-center gap-1 bg-violet-600 hover:bg-violet-600/90 w-[120px] h-[40px] rounded-lg"
+        onClick={() => navigate('/account')}
+      >
+        <MdAccountCircle />
+        My account
+      </button>
+    </div>
+  );
+};
+
+export const Header = ({
+  setNavBarActive,
+}: {
+  setNavBarActive: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const pages = getPages();
+  const navigate = useNavigate();
+
+  const handleNavBarToggle = () => setNavBarActive((prev) => !prev);
 
   return (
     <div
@@ -33,7 +77,7 @@ export const Header = () => {
                   ? 'text-violet-500/90'
                   : ''
               }`}
-              onClick={handlePageSwitch(pagePath)}
+              onClick={() => navigate(pagePath)}
             >
               {pageName}
             </span>
@@ -41,12 +85,17 @@ export const Header = () => {
       </div>
       <button
         data-testid="my-account-button"
-        className="bg-violet-600 hover:bg-violet-600/90 p-2 rounded-lg max-sm:hidden"
-        onClick={handlePageSwitch('/account')}
+        className="flex items-center gap-1 bg-violet-600 hover:bg-violet-600/90 p-2 rounded-lg max-sm:hidden"
+        onClick={() => navigate('/account')}
       >
+        <MdAccountCircle />
         My account
       </button>
-      <div className="hidden max-sm:block" data-testid="hamburger-menu">
+      <div
+        className="hidden max-sm:block"
+        data-testid="hamburger-menu"
+        onClick={handleNavBarToggle}
+      >
         <GiHamburgerMenu size={30} />
       </div>
     </div>
@@ -79,6 +128,8 @@ interface IProps {
 }
 
 export const Layout = ({ title, children }: IProps) => {
+  const [navBarActive, setNavBarActive] = useState(false);
+
   if (typeof title !== 'undefined') window.document.title = title;
 
   return (
@@ -86,7 +137,7 @@ export const Layout = ({ title, children }: IProps) => {
       data-testid="layout"
       className="w-screen min-h-screen flex flex-col bg-black/95 break-all"
     >
-      <Header />
+      <Header setNavBarActive={setNavBarActive} />
 
       <div className="flex flex-col gap-10 container mx-auto my-10">
         {title && (
@@ -96,6 +147,8 @@ export const Layout = ({ title, children }: IProps) => {
         )}
         {children}
       </div>
+
+      {navBarActive && <Navbar setNavBarActive={setNavBarActive} />}
 
       <Footer />
     </div>
