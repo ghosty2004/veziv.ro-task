9b9dc48864a8a9d4652382a5b61d48729267c774
ghosty2004 <fejerszilveszter@gmail.com>
2023-12-03 13:58:29 +0200

    changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// Modules
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersPortfoliosModule } from './users-portfolios/users-portfolios.module';
-import { join } from 'node:path';
+import { AuthModule } from './auth/auth.module';
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../', 'client', 'dist'),
+      renderPath: '/',
     }),
     DatabaseModule,
+    AuthModule,
     UsersModule,
     UsersPortfoliosModule,
   ],
