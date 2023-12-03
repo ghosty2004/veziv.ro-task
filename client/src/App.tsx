891af313a907465a227c08d532404b504b3b38d0
ghosty2004 <fejerszilveszter@gmail.com>
2023-12-03 13:58:59 +0200

    added react query and axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { NotFound } from '@/components';
 import { getPages } from '@/utils';
 import { ContextProvider } from './context';
+
+const queryClient = new QueryClient();
 
 function App() {
   const pages = getPages();
 
   return (
-    <ContextProvider>
-      <Routes>
-        {pages.map(({ Component, pagePath }, index) => (
-          <Route key={index} path={pagePath} element={<Component />} />
-        ))}
+    <QueryClientProvider client={queryClient}>
+      <ContextProvider>
+        <Routes>
+          {pages.map(({ Component, pagePath }, index) => (
+            <Route key={index} path={pagePath} element={<Component />} />
+          ))}
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </ContextProvider>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ContextProvider>
+    </QueryClientProvider>
   );
 }
 
