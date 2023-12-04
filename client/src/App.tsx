58e37bc886dad5c9b24f0c0466f1b837f407d46d
ghosty2004 <fejerszilveszter@gmail.com>
2023-12-04 17:23:23 +0200

    added dependency react toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'react-toastify';
 import { NotFound } from '@/components';
 import { getPages } from '@/utils';
 import { ContextProvider } from './context';
 import * as pagesComponents from './pages';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient();
 
@@ -11,21 +14,24 @@ function App() {
   const pages = getPages();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ContextProvider>
-        <Routes>
-          {Object.entries(pagesComponents)
-            .map(([pageName, Component]) => ({
-              ...pages.find((f) => f.pageName === pageName)!,
-              Component,
-            }))
-            .map(({ pagePath, Component }, index) => (
-              <Route key={index} path={pagePath} element={<Component />} />
-            ))}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </ContextProvider>
-    </QueryClientProvider>
+    <div className="overflow-x-hidden">
+      <QueryClientProvider client={queryClient}>
+        <ContextProvider>
+          <Routes>
+            {Object.entries(pagesComponents)
+              .map(([pageName, Component]) => ({
+                ...pages.find((f) => f.pageName === pageName)!,
+                Component,
+              }))
+              .map(({ pagePath, Component }, index) => (
+                <Route key={index} path={pagePath} element={<Component />} />
+              ))}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </ContextProvider>
+      </QueryClientProvider>
+      <ToastContainer position="bottom-right" className="mb-14" theme="dark" />
+    </div>
   );
 }
 
